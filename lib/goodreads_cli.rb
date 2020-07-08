8919ee6bf0e11274ec8e999ee52c5bb1842b2e17
joannesun44 <joannesun44@gmail.com>
2020-07-08 00:27:25 -0400

    first commit

---
+++ lib/goodreads_cli.rb
@@ -0,0 +1,6 @@
+require "goodreads_cli/version"
+
+module GoodreadsCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/goodreads_cli/version.rb
@@ -0,0 +1,3 @@
+module GoodreadsCli
+  VERSION = "0.1.0"
+end
